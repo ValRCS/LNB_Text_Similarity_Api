d8d5e38971e50bc537d76c42fc4aff40b97a90ae
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-10-26 18:08:01 +0300

    updated with data

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,15 +1,47 @@
-import os
+# import os
 
 from flask import Flask, jsonify, request
+import pandas as pd
+import numpy as np
 
+def find_merge_index(df, term_list=()):
+    index_list = []
+    for term in term_list:
+        iarr = np.where(df.index==term)[0]
+        index_list.append(iarr)
+    c = np.concatenate(index_list)
+    c.sort(kind='mergesort')
+    return c
+
+def find_close_indexes(iarr, terms=3, window=5):
+    close_list = [(iarr[n],iarr[n+1],iarr[n+2]) for n in range(len(iarr)-terms+1) if (iarr[n+2]-iarr[n]) <= window]
+    return close_list
+
+def return_fragment(df, close_index, padding=10):
+    beg = close_index[0]
+    end = close_index[-1]
+    d = {
+        "title": df.iloc[beg].fname,
+        "text": " ".join(df.iloc[beg-padding:end+padding].form.values),
+        "lemma": " ".join(df.iloc[beg-padding:end+padding].lemma.values),
+        "upos": " ".join(df.iloc[beg-padding:end+padding].upos.values)
+    }
+    return d
+
+def return_top_fragments(df, clist, num_frags=5, padding=10, offset=2):
+    frag_list = [return_fragment(df, close_index, padding) for close_index in clist[offset:num_frags+offset]]
+    return frag_list
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
 
+    df = pd.read_parquet('../data/lima_lemma_index_20222610.parquet') # FIXME to configurable path
+
+
     # app = Flask(__name__, instance_relative_config=True)
     # app.config.from_mapping(
-    #     SECRET_KEY='dev',
+    #     SECRET_KEY='dev', # FIXME: change this to a random value when deploying
     #     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     # )
 
@@ -36,15 +68,13 @@ def create_app(test_config=None):
 
     @app.route('/search/<t1>/<t2>/<t3>/', methods = ['GET'])
     def disp(t1,t2,t3):
+        term_list = [t1,t2,t3]
+        iarr = find_merge_index(df, term_list)
+        clist = find_close_indexes(iarr)
+        frag_list = return_top_fragments(df, clist)
+        return jsonify({'search_terms': f"{t1} {t2} {t3}",
+        'fragments': frag_list})
     
-        return jsonify({
-            'search_terms': f"{t1} {t2} {t3}",
-            'results': [
-                {"domID": "1", "title": "title1", "url": "url1", "snippet": "snippet1"},
-                {"domID": "2", "title": "title2", "url": "url2", "snippet": "snippet2"}
-            ]   
-        })
-
     @app.errorhandler(404)
     def page_not_found(e):
     # note that we set the 404 status explicitly
