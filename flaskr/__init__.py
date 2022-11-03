3d6e1937b2f520ce0357a882efe9dc057ed691a6
Valdis Saulespurēns <valdis.saulespurens@lnb.lv>
2022-11-03 14:35:20 +0200

    Added search prototype

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask, jsonify, request, render_template, url_for
 import pandas as pd
 import numpy as np  
 from pathlib import Path
+from datetime import datetime
 
 def load_all_chunks_as_df(pattern='data/lima_20221101_*.parquet'):
     df = pd.concat([pd.read_parquet(f) for f in Path().glob(pattern)])
@@ -37,6 +38,42 @@ def return_fragment(df, close_index, padding=10):
 def return_top_fragments(df, clist, num_frags=5, padding=10, offset=2):
     frag_list = [return_fragment(df, close_index, padding) for close_index in clist[offset:num_frags+offset]]
     return frag_list
+
+def create_pattern(word_tuple, ending_replace_dict, window=100):
+    """Create regex pattern from word_tuple and ending_replace_dict"""
+    # create pattern from word_tuple
+    word_list = []
+    for word in word_tuple:
+        if word[-1] in ending_replace_dict:
+            word = word[:-1] + ending_replace_dict[word[-1]]
+        word_list.append(word)
+    pattern = (".{1," + str(window) + "}").join(word_list)
+    return pattern
+
+ending_replace_dict = {
+    "l": "[lļ]",
+    "r": "[rŗ]",
+    "n": "[nņ]",
+    "s": "[sš]",
+    "z": "[zž]",
+    "j": "[jģ]",
+    "c": "[cč]",
+    "d": "[dķ]"
+}
+
+def find_pattern(df, word_tuple, window=100, verbose=True, ending_replace_dict=ending_replace_dict):
+    pattern = create_pattern(word_tuple, ending_replace_dict, window=window)
+    if verbose:
+        print("Will look for Pattern:", pattern)
+        start_datetime = datetime.now()
+        print("Starting search at:", start_datetime)
+        
+    found_df = df[df["lower_text"].str.contains(pat=pattern, case=False)]
+    if verbose:
+        end_datetime = datetime.now()
+        print("Found", found_df.shape[0], "rows at:", end_datetime)
+        print("Search took:", end_datetime - start_datetime)
+    return found_df.index.to_list()
 
 def create_app(test_config=None):
     # create and configure the app
@@ -100,6 +137,23 @@ def create_app(test_config=None):
         text = plaintext_df.loc[fname,"text"]
         # pass text to jinja template
         return render_template('plaintext.html', text=text, fname=fname)
+
+    # serve search page
+    @app.route('/search', methods = ['GET'])
+    def search():
+        return render_template('search.html')
+
+    # process search results
+    @app.route('/search', methods = ['POST'])
+    def search_post():
+        # get form data
+        terms = request.form['terms']
+        # split terms into list
+        term_list = terms.split()
+        # find indexes
+        # todo make window adjustable
+        pattern_results = find_pattern(plaintext_df, term_list, window=50, verbose=True)
+        return render_template('search.html', pattern_results=pattern_results)
         
     
     @app.errorhandler(404)
